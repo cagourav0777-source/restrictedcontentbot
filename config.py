54d74839937afcad1387295612ad635eff18d3f8
Gourav kumar <cagourav0777@gmail.com>
2026-07-19 16:34:46 +0530

    Set default values for Redis configuration

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ class Telegram:
     
 class Database:
     """Redis database configuration."""
-    REDIS_HOST = os.environ.get('REDIS_HOST')
-    REDIS_PORT = int(os.environ.get('REDIS_PORT'))
-    REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+    REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+    REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+    REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
